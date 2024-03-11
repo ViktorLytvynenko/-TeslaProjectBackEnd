51b56015bb5e0b6105f284f0687f9614c7bfe4d5
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-03-11 10:47:17 +0200

    added

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:
+        `mongodb+srv://admin:admin@cluster.k3ipxms.mongodb.net/tesla?retryWrites=true&w=majority`,
+    secretOrKey: 'random very very secret string',
+};
